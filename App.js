d559fd60ce4922c6e496d58309fd8cd1aca524d5
Joshua Niederer <josh@jpniederer.com>
2017-10-19 22:48:10 -0500

    Setting up base for notification.

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import DeckList from './components/DeckList';
 import Header from './components/Header';
+import { Constants } from 'expo';
+import { red } from './utils/colors';
+import { setLocalNotification } from './utils/notificationSetup';
+
+function FlashStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} { ...props} />
+    </View>
+  )
+}
 
 const MainNavigator = StackNavigator({
   Home: {
@@ -15,7 +26,7 @@ const MainNavigator = StackNavigator({
 
 export default class App extends React.Component {
   componentDidMount() {
-
+    setLocalNotification();
   }
 
   // store={createStore(reducer)}
@@ -23,9 +34,7 @@ export default class App extends React.Component {
     return (
       <Provider>
         <View style={styles.container}>
-          <Header>
-            MobileFlashcards
-          </Header>
+          <FlashStatusBar backgroundColor={red} barStyle='light-content' />
           <Text>This is MobileFlashcards</Text>
           <MainNavigator />
         </View>
